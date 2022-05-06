705571a896ed2cf45f6515d438cf7b6fcf5dec01
Ashish <ash.ishu025g@gmail.com>
2022-05-06 22:31:24 +0530

    variables

--- CoreJS/eventloop.js
+++ CoreJS/eventloop.js
@@ -26,7 +26,21 @@ setTimeout(function() {
 console.log("We did it!")
 
 
+const arr = [10, 12, 15, 21];
+for (var i = 0; i < arr.length; i++) {
+  setTimeout(function(i_local) {
+    return function () {
+        console.log('Index: ' + i_local + ', element: ' + arr[i_local]);    
+    }    
+  }(i), 3000);
+}
 
+//const arr = [10, 12, 15, 21];
+// for (let i = 0; i < arr.length; i++) {
+//   setTimeout(function() {
+//     console.log('Index: ' + I_local + ', element: ' + arr[I_local]);    
+//   }, 3000);
+// }
 
 
 
