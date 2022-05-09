4a1c480a8f3773cccae6a89e009895cecb5f6099
Ashish <ash.ishu025g@gmail.com>
2022-05-09 22:27:34 +0530

    default params

--- ES6/defaultParams.js
+++ ES6/defaultParams.js
@@ -12,13 +12,20 @@ function GetVehicleDetails(name = "Default Vehicle", price = "$0", brandValue = 
     `)
 }
 
-GetVehicleDetails();
-GetVehicleDetails("BMW", "$5000", "Amazing");
-GetVehicleDetails("Toyota","$5000");
+//GetVehicleDetails();
+//GetVehicleDetails("BMW", "$5000", "Amazing");
+//GetVehicleDetails("Toyota","$5000");
 
 
 function Divide(param1 = 1, param2 = 1) {
     //return isNaN(param1/param2);
+    // if (typeof param1 != undefined && typeof param2 != undefined) {
+        
+    // }
+    //let output =  param1 && param2 ? param1/param2 : "The values are not provided"
+
+    //return output;
+    
     return param1/param2;
 }
 
