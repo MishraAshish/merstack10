40d0229d3f8adb7082cf162c468f170b3ec6ba25
Ashish <ash.ishu025g@gmail.com>
2022-05-12 22:57:01 +0530

    arrow function

--- ES6/spreadandrest.js
+++ ES6/spreadandrest.js
@@ -46,7 +46,7 @@ let Accounts ={
 
 //let PurhaseOrder = {Accounts,  DeliveryAddress : "address3"};
 let PurhaseOrder = {...Accounts,  deliveryAddress : "address3"};
-console.log(PurhaseOrder)
+//console.log(PurhaseOrder)
 
 Accounts.accountHolderName = "Toan";
 //console.log(PurhaseOrder)
@@ -64,10 +64,29 @@ function useRestParams(y=0, ...params){
 
 let arraNm = [1,2,3,4]
 
-useRestParams(...arraNm)
+//useRestParams(...arraNm)
 
 //create a function which accepts start and end of number and generates a array of that size, [100....150]
 //then use this array to pass as spread operator into a function named largesum
 //in largesum we should accept the array in rest parameter (...arrayOfNums), and then add the numbers
 // generatearray(start, end)
 // largesum(...arrayNum) and returns sum
+
+function largesum(...params){ //array rest
+    //[1,2,3]
+    let summ = params.reduce((prevValue, currentValue, cuurentIndex, arr)=>{
+        //console.log(prevValue)
+        //console.log(currentValue)
+        //console.log(cuurentIndex)
+
+         return prevValue+currentValue;
+     },0);
+ 
+     return summ;
+ }
+
+ //console.log(largesum(...arrayNum))
+
+ let newArr = [3242,32423,23423,32423,23423,434234,32423,423423423,423,423,4234,23423,423,423,423,423];
+
+ console.log("Using Rest " + largesum(...newArr)) //array spread
