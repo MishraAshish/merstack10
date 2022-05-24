e7a0a9b1490e7291565dd869fdf9ae6de047e642
Ashish <ash.ishu025g@gmail.com>
2022-05-24 23:27:33 +0530

    props props.children state setstate forceupdate eventbinding prurecomponent

---
+++ reactapp/src/app/CommonComponents/DummyComponent.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+//props : is used to share data between components, its the only way
+let DummyComponent = (props) => {
+    //props are immutable, as they belong to parent component
+    //props.userName = "Toan Ng";//will not be allowed
+    let dataFromParent = props;
+    console.log("Dummy Component")
+    return(
+        <>
+        <h1>Dummy Component From Common Component</h1>
+            <p>{dataFromParent.userName}</p>
+            <p>{dataFromParent.userAddress}</p>
+
+            {props.children && props.children[0]}
+            {props.children && props.children[1]}
+        </>
+    )
+} 
+
+export default DummyComponent;
--- reactapp/src/app/app.js
+++ reactapp/src/app/app.js
@@ -1,20 +1,75 @@
-import React, { Component } from "react"; //React - default import , Component - named import
+import React, { Component, PureComponent } from "react"; //React - default import , Component - named import
 
-import FooterComponent, {DummyComponent, pi} from "./CommonComponents/FooterComponent";
+import FooterComponent, {pi} from "./CommonComponents/FooterComponent";
+import DummyComponent from "./CommonComponents/DummyComponent";
 
-export default class AppComponent extends Component //React.Component 
+
+//export default class AppComponent extends Component //React.Component 
+//Pure Component contains predefined implementation of ShouldComponentUpate
+export default class AppComponent extends PureComponent
 {
+    constructor(props){
+        super(props);
+
+        //state object is used to allow changes with the help of predefined api's in react 
+        //(setState and forceUpdate)
+        this.state = {
+            name : "Default Name",
+            address : "Default Address"
+        }
+    }
+
+    //whenever we define and event listner javascript provide event object bydefault(evt)
+    handleClick = (evt )=>{
+        //react needs to re-render to show the changed values (state)
+        //re-render can only be done when we change the state with predefined api's (setstate, forceupdate)
+        
+        //using setState allows us to go with the lifecycle methods of component
+        //the state changes are done with api in batch process manner
+        this.setState({
+            name : "Toan",
+            address : "Somewhere in vietname"
+        },()=>console.log("state is updated"))
+
+        console.log("state change value ", this.state) //? immediately we dont see the update
+        //alert("We will change the state values")
+
+        //we should avoid using forceUpdate to update the state, as it skips the life cycle methods
+        //this.state.name = "TN",
+        //this.forceUpdate();
+
+        evt.preventDefault();//preventing the default behaviour of the html element
+    }
+
+    //update life cycle method
+    // shouldComponentUpdate(nextState, nextProps){
+    //     console.log("nextState ", nextState)
+    //     console.log("nextProps ", nextProps)
+    //     if (this.state.name == nextProps.name) {
+    //         return false
+    //     } 
+
+    //     return true;
+    // }
 
     render(){
+        console.log("Render in app component")
         let address = "Somewhere on earth"; //js variable
         //this returns virtual dom (view) and it should have only one parent returned
-        //JSX - javascript like xml structure
+        //JSX - javascript like xml structure -> virtual dom
         return( 
             <div>
                 <h1>Application Component</h1> 
                 <p><b>{address +"  "+ pi}</b></p>
                 <FooterComponent></FooterComponent>
-                <DummyComponent/>
+                {/* <DummyComponent userName={"Toan"} userAddress={"Somewhere in US"}/> */}
+
+                <DummyComponent userName={this.state.name} userAddress={this.state.address} >
+                    {/* <h1>{"The First Header From Parent App Component"}</h1> */}
+                    <FooterComponent/>
+                    <h2>{"The Second Header From Parent App Component"}</h2>
+                </DummyComponent>
+                <button onClick={this.handleClick} >Click To Update</button>
             </div>            
         )
     }
