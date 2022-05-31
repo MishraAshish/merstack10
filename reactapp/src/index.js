c8fc07a8e30fe515a08fd483267a14ed82302ef4
AshisMishra <ash.ishu025@gmail.com>
2022-05-31 23:52:02 +0530

    redux-thunk promisemiddleware

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,10 +1,14 @@
 console.log("Entry Point of our application");
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./app/State/store";
 
 import AppComponent from "./app/app";
 
 render(
-    <AppComponent/>,//which application you want to render,
+    <Provider store={store}>
+        <AppComponent/>
+    </Provider>,//which application you want to render,
     document.getElementById("root") //bootstrapping of application in root element of index.html
 )
