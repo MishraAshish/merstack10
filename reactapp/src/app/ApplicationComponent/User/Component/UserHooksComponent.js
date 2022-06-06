91ab2979d7ec94ee782e565b5c950dbcb67372c1
AshisMishra <ash.ishu025@gmail.com>
2022-06-06 23:31:52 +0530

    useState useRef useSelector useDispatch useEffect

--- reactapp/src/app/ApplicationComponent/User/Component/UserHooksComponent.js
+++ reactapp/src/app/ApplicationComponent/User/Component/UserHooksComponent.js
@@ -1,40 +1,83 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useRef, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";//replacement of mapStateToProps mapDispatchToProps
 
+import { signinSignupUser } from "../../../State/User/UserActions";
 
-let User = (props)=>{
+let User = (props)=>{   
 
-    // constructor(){
-    //     this.state = {
-    //         userName : "Toan"
-    //     }
-    // }
-    // onChange = (evt)=>{
-    //     this.setState({userName : evt.target.vale})
-    // }
+    //by using useRef we can create reference for html elements to be updated without adding state change
+    //let refInput = react.createRef()
+    // creating inputs with the help of ref keyword with userRef function
+    let inputUserName = useRef(null);
+    let inputPassword = useRef(null);
+    let inputMobile = useRef(null);
+    let inputStreet = useRef(null);
 
-    // this is a destructure that we get from useState, first is the value of the state (userName), initialized in useState
-    // second one is the callBack function to allow us to update the state
-    let [userName, setUserName] = useState("Toan"); // the first hook to update the react virtual dom callrender
+    //useSelector allows to subscribe from store (here reading user data)
+    let user = useSelector((state)=>state.userReducer.user);
     
-    //let [address, setUserAddress] = useState({"Address1":"Value1", "Address2": "value2"});
+    //userName = userObject.userName; // we can't do it as rendering is not done yet so no access to html element
 
-    let onChangeUserName = (evt)=>{
-        //let userName = evt.target.value;
-        console.log(evt.target.value)
-        setUserName(evt.target.value)
+    //a hook that here gets called on first render - componentDidMount, we can access the view and assign the value to it
+    useEffect(()=>{
+        inputUserName.current.value = user.userName;//
+        inputPassword.current.value = user.password;
+        inputStreet.current.value = user.street;
+        inputMobile.current.value = user.mobile;
+    })
+
+    //in place of mapDispatch to props to publish the data to store we are using - useDispatch
+    let dispatchToSaveUser = useDispatch();
+
+    //console.log(userObject)
+    let readFormData = (evt)=>{
+        // `current` points to the mounted text input element
+        
+        let userObj = {
+            userName : inputUserName.current.value,
+            password : inputPassword.current.value,
+            street : inputStreet.current.value,
+            mobile : inputMobile.current.value
+        }
+        alert("User Object :" + JSON.stringify(userObj))
+
+        //dispatching sigin method which will further dispatch addUser action to store
+        dispatchToSaveUser(signinSignupUser(userObj))
+
+        evt.preventDefault();
     }
 
     return(
         <>
             <h1>User Component Hooks</h1>
 
-            <form className={"form col-md-10 userHook"}>                
+            <form className={"form col-md-10 userHook"} onSubmit={readFormData}>                
                 <label>
                     <b>User Name :</b>
-                    <input type="text" className={"form-control col-md-12"} value={userName} 
-                        onChange={onChangeUserName}
-                            placeholder="Please enter user name" maxLength={20} required/>
+                    <input type="text" className={"form-control col-md-12"} ref={inputUserName} 
+                        placeholder="Please enter user name" maxLength={20} required/>
+                </label>
+                <br/>
+                <label>
+                    <b>Password :</b>
+                    <input type="password" className={"form-control col-md-12"} ref={inputPassword} 
+                            placeholder="Please enter password" maxLength={20} required/>
+                </label>
+                <br/>
+                <label>
+                    <b>Street :</b>
+                    <input type="text" className={"form-control col-md-12"} ref={inputStreet} 
+                            placeholder="Please enter address" maxLength={20}/>
+                </label>
+                <br/>
+                <label>
+                    <b>Mobile :</b>
+                    <input type="number" className={"form-control col-md-12"} ref={inputMobile} 
+                            placeholder="Please enter mobile" />
                 </label>                
+
+                <br/>
+                <input type="submit" className={"btn btn-primary"} value="Signin" />
             </form>
         </>
     )
